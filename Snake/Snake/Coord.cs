eecbdfbb97dc2087c3412c065aac998b94eb5079
Thomas Stroemme <strtho@gmail.com>
2012-10-21 16:57:38 +0200

    added code Coord.cs and Snake.cs

--- Snake/Snake/Coord.cs
+++ Snake/Snake/Coord.cs
@@ -5,7 +5,21 @@ using System.Text;
 
 namespace Snake
 {
-    class Coord
+    struct Coord
     {
+        public int posX;
+        public int posY;
+
+        public Coord(int x, int y)
+        {
+            posX = x;
+            posY = y;
+        }
+
+        public Coord(Coord input) 
+        { 
+            posX = input.posX; 
+            posY = input.posY; 
+        }
     }
 }
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -5,7 +5,69 @@ using System.Text;
 
 namespace Snake
 {
-    class Snake
+    class Snake:List<Coord>
     {
+        
+        public Coord tail, head, newHead;
+        Random random;
+        List<Coord> snake;
+        GameDataLogic gdl;
+
+        // Konstruktør
+        public Snake()
+        {
+            setupSnake(coord);
+        }
+
+
+        public List<Coord> bodyPostition()
+        {
+            return 0;
+        }
+
+        void grow(int x, int y)
+        { 
+            
+        }
+
+        public Coord updatePosition(Coord)
+        {
+            return 0;
+        }
+
+        public void getBodyPosition()
+        {
+        
+        }
+
+        public bool hasCoord()
+        {
+            return false;
+        }
+
+        public Coord setupSnake(Coord coord)
+        {
+
+            // Initialiserer
+            random = new Random();
+            coord = new Coord(10,10);
+            
+            snake = new List<Coord>();
+
+            // Legger til slangen
+            snake.Add(coord);
+            snake.Add(coord);
+            snake.Add(coord);
+            snake.Add(coord);
+
+            tail = new Coord(snake.First());
+            head = new Coord(snake.Last());
+            newHead = new Coord(head);
+
+            if (!gg)
+            {
+                snake.Add(newHead);
+            }
+        }
     }
 }
